365b8dcad62c21fa7f9869f121aa2d7cb057d72d
Stephane NANGUE <stephaneleonel@gmail.com>
2024-06-16 21:47:01 +0200

    new clickhouse audit device

--- Lab-04/vault/terraform/vault-cluster-config/module/audit.tf
+++ Lab-04/vault/terraform/vault-cluster-config/module/audit.tf
@@ -8,3 +8,14 @@ resource "vault_audit" "s3_device" {
   }
   path          = "s3_device/"
 }
+
+resource "vault_audit" "clickhouse_device" {
+  type          = "socket"
+  description   = "audit logs in ClickHouse"
+  local         = false
+  options = {
+    address     = "127.0.0.1:5170"
+    socket_type = "tcp"
+  }
+  path          = "clickhouse_device/"
+}
