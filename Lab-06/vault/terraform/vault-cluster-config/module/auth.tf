1e1d7bcbb38876bfa717252d187fa705c7ec4862
Stephane NANGUE <stephaneleonel@gmail.com>
2025-02-27 23:46:23 +0100

    zitadel and postgres

--- Lab-06/vault/terraform/vault-cluster-config/module/auth.tf
+++ Lab-06/vault/terraform/vault-cluster-config/module/auth.tf
@@ -21,27 +21,3 @@ resource "vault_ldap_auth_backend_group" "group" {
     policies  = [vault_policy.admin.name]
     backend   = vault_ldap_auth_backend.ldap.path
 }
-
-resource "vault_jwt_auth_backend" "oidc" {
-    description         = "Demonstration of the Terraform JWT auth backend"
-    path                = "oidc"
-    type                = "oidc"
-    oidc_discovery_url  = "https://zitadel.openlab.net"
-    oidc_client_id      = "TO_MODIFY"
-    oidc_client_secret  = "TO_MODIFY"
-    default_role = "admin"
-}
-
-resource "vault_jwt_auth_backend_role" "admin" {
-  backend         = vault_jwt_auth_backend.oidc.path
-  role_name       = "admin"
-  token_policies  = ["default", "admin"]
-
-  bound_claims = {
-    email = "vaultops@zitadel.oplenlab.net, root@zitadel.zitadel.openlab.net"
-  }
-  oidc_scopes           = ["email"]
-  user_claim            = "email"
-  role_type             = "oidc"
-  allowed_redirect_uris = ["https://vault.openlab.net/ui/vault/auth/oidc/oidc/callback"]
-}
--- Lab-06/zitadel/README.md
+++ Lab-06/zitadel/README.md
@@ -28,4 +28,26 @@ the default user is root@zitadel.zitadel.openlab.net
     "user_claim": "email"
 }
 
-hvs.YsvRlFfvF8xHLgsz9sRFjnXh
+resource "vault_jwt_auth_backend" "oidc" {
+    description         = "Demonstration of the Terraform JWT auth backend"
+    path                = "oidc"
+    type                = "oidc"
+    oidc_discovery_url  = "https://zitadel.openlab.net"
+    oidc_client_id      = "TO_MODIFY"
+    oidc_client_secret  = "TO_MODIFY"
+    default_role = "admin"
+}
+
+resource "vault_jwt_auth_backend_role" "admin" {
+  backend         = vault_jwt_auth_backend.oidc.path
+  role_name       = "admin"
+  token_policies  = ["default", "admin"]
+
+  bound_claims = {
+    email = "vaultops@zitadel.oplenlab.net, root@zitadel.zitadel.openlab.net"
+  }
+  oidc_scopes           = ["email"]
+  user_claim            = "email"
+  role_type             = "oidc"
+  allowed_redirect_uris = ["https://vault.openlab.net/ui/vault/auth/oidc/oidc/callback"]
+}
